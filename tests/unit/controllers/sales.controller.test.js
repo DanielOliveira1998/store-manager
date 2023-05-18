02556d21bba7a7f6c34debd4d6138ab6d44b7242
Daniel Oliveira <danoliveira865@gmail.com>
2023-05-17 21:59:59 -0300

    retorna testes

--- tests/unit/controllers/sales.controller.test.js
+++ tests/unit/controllers/sales.controller.test.js
@@ -3,19 +3,3 @@ const { expect } = chai;
 const sinon = require('sinon');
 const salesService = require('../../../src/services/sales.services');
 const salesController = require('../../../src/controllers/sales.controller');
-
-// describe('Controller Sales Test', () => {
-//   describe('Verifica sucessos', () => {
-//     it('Verifica deleção', async () => {
-//       const req = {
-//         params: { id: 1 }
-//       };
-//       const res = {};
-//       res.status = sinon.stub().returns(res);
-//       res.json = sinon.stub().returns();
-//       sinon.stub(salesService, 'excludeSale').resolves(1)
-//       const result = await salesController.excludeSale(req, res);
-//       expect(result).to.be.equal(null);
-//     })
-//   });
-// })
